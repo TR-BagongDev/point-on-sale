b45080e98ecaa5c62a525d4614697d7b4f02ce99
TR-BagongDev <matias.indra@tristansolutech.co.id>
2026-02-24 21:49:41 +0700

    auto-claude: subtask-2-1 - Update app/kasir/page.tsx to remove DashboardLayout

--- app/kasir/page.tsx
+++ app/kasir/page.tsx
@@ -1,10 +1,5 @@
-import { DashboardLayout } from "@/components/layout/DashboardLayout";
 import { KasirClient } from "./KasirClient";
 
 export default function KasirPage() {
-  return (
-    <DashboardLayout>
-      <KasirClient />
-    </DashboardLayout>
-  );
+  return <KasirClient />;
 }
